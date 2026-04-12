c429e1f07cbc6d7671335da9f68d7e932e9e821c
Lua <luanatferreirac@gmail.com>
2026-04-12 18:05:02 -0300

    Fix read function to capture user input correctly

--- multy.c
+++ multy.c
@@ -3,5 +3,7 @@ int main(){
 char primeiro_char[3], segundo_char[3];
 int primeiro_int, segundo_int;
 write(1, "Primeiro número: ", 3);
-read(0, "Segundo número: ", 3);
+read(0, primeiro_char, 3);
+write(1, "Segundo número: ", 3);
+read(0, segundo_char, 3);
 }
