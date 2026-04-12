eae5623a9c28e5f9c51ddac6ed5aea9f669dc535
Lua <luanatferreirac@gmail.com>
2026-04-12 18:13:09 -0300

    Prompt user for two integers
    Added prompts to read two integers from user input.

--- multy.c
+++ multy.c
@@ -1,9 +1,14 @@
 #include <unistd.h>
+
 int main(){
+
 char primeiro_char[3], segundo_char[3];
 int primeiro_int, segundo_int;
+
 write(1, "Primeiro número: ", 3);
 read(0, primeiro_char, 3);
 write(1, "Segundo número: ", 3);
 read(0, segundo_char, 3);
+
+
 }
