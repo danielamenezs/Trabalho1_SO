dd358a8667eb81fadd4e3260af54c9d645af4057
Daniela <danielamenezes483@gmail.com>
2026-04-12 20:31:20 -0300

    Implement multiplication and file output in main

--- multiplicação.c
+++ multiplicação.c
@@ -47,3 +47,54 @@ int juntarTexto(char *mensagem, int posicaoAtual, char *trecho) {
 
     return posicaoAtual;
 }
+
+int main() {
+    char entradaA[20];
+    char entradaB[20];
+    char numeroATexto[20];
+    char numeroBTexto[20];
+    char resultadoTexto[20];
+    char mensagem[100];
+
+    int numeroA;
+    int numeroB;
+    int resultado;
+    int bytesLidosA;
+    int bytesLidosB;
+    int arquivo;
+    int tamanhoMensagem = 0;
+
+    write(1, "digite o primeiro numero: ", 26);
+    bytesLidosA = read(0, entradaA, 19);
+    if (bytesLidosA <= 1) return 1;
+    entradaA[bytesLidosA - 1] = '\0';
+
+    write(1, "digite o segundo numero: ", 25);
+    bytesLidosB = read(0, entradaB, 19);
+    if (bytesLidosB <= 1) return 1;
+    entradaB[bytesLidosB - 1] = '\0';
+
+    numeroA = transformarTextoEmNumero(entradaA);
+    numeroB = transformarTextoEmNumero(entradaB);
+    resultado = numeroA * numeroB;
+
+    transformarNumeroEmTexto(numeroA, numeroATexto);
+    transformarNumeroEmTexto(numeroB, numeroBTexto);
+    transformarNumeroEmTexto(resultado, resultadoTexto);
+
+    tamanhoMensagem = juntarTexto(mensagem, tamanhoMensagem, "resultado da multiplicacao: ");
+    tamanhoMensagem = juntarTexto(mensagem, tamanhoMensagem, numeroATexto);
+    tamanhoMensagem = juntarTexto(mensagem, tamanhoMensagem, " x ");
+    tamanhoMensagem = juntarTexto(mensagem, tamanhoMensagem, numeroBTexto);
+    tamanhoMensagem = juntarTexto(mensagem, tamanhoMensagem, " = ");
+    tamanhoMensagem = juntarTexto(mensagem, tamanhoMensagem, resultadoTexto);
+    tamanhoMensagem = juntarTexto(mensagem, tamanhoMensagem, "\n");
+
+    write(1, mensagem, tamanhoMensagem);
+
+    arquivo = open("resultado.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    write(arquivo, mensagem, tamanhoMensagem);
+    close(arquivo);
+
+    return 0;
+}
